c8393003f4c8a7565731216d715bb2dd3691ce2e
bridgebook112 <bridgebook1999@gmail.com>
2021-11-01 15:43:34 +0900

    たぶん完成

--- xldiff.bat
+++ xldiff.bat
@@ -1,13 +1,14 @@
+@ECHO OFF
 set path1=%1
 set path2=%2
 REM Create paths to original and current spreadsheets to store in tmp
 REM Change forward slash to back slash on all paths for the Excel tool
 set path1=%path1:/=\%
 set path2=%path2:/=\%
-REM Clean up paths and write to tmp file as spreadsheetcompare requires input to be passed inside of a file.
-dir %path1% /B /S > tmp.txt
-dir %path2% /B /S >> tmp.txt
 if not "%path1:xls=%" == "%path1%" (
+ REM Clean up paths and write to tmp file as spreadsheetcompare requires input to be passed inside of a file.
+ dir %path1% /B /S > tmp.txt
+ dir %path2% /B /S >> tmp.txt
  "C:\Program Files (x86)\Microsoft Office\root\Office16\DCF\spreadsheetcompare" tmp.txt
 ) else (
  "C:\Program Files\WinMerge\WinMergeU.exe" %1 %2
